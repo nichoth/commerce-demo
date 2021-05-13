d5d79975dc083c35d56becf33d317824e10621cb
nichoth <nichoth@gmail.com>
2021-05-13 11:42:57 -0700

    trying things

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log('woooo')
+import Commerce from '@chec/commerce.js';
 
+var pubKey = 'pk_1832690656a0cfa0f61589546f588413b8866fc98f7a6'
+
+const commerce = new Commerce(pubKey);
+
+commerce.products.list().then(products => {
+    console.log('products', products)
+});
+
+console.log('woooo')
